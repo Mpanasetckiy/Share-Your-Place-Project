662f39db42a617237000bf6cb639c39a71f37611
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-05-13 00:52:49 +0100

    feat(frontend): add responsive navigation bar with side drawer
    This commit adds a responsive navigation bar to the frontend using
    React components. The navigation bar includes a hamburger menu button
    that opens a side drawer on smaller screens. The navigation bar also
    includes links to the home page, a page to add a new place, and a login
    page. The side drawer includes the same links as the navigation bar.
    
    The commit also adds a backdrop component that is used to darken the
    background when the side drawer is open.

---
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import "./MainNavigation.css";
+
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav"></nav>
+          <NavLinks />
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">My places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
+
+export default MainNavigation;
