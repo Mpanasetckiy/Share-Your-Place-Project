7d62cf42b611cd64812ffb9dab49389f7c9f6587
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-05-12 23:56:17 +0100

    feat(UserItem.css, Avatar.css, Avatar.js, Card.css, Card.js, UsersList.css, UsersList.js, Users.jsx): add styles and components for user list and user item. Add UsersList component to Users page.

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users page</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Maksim Lukianenko",
+      image: "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
