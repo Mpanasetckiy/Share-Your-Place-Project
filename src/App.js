8dc540e40e28bdff99c172328a82cac98747e850
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-06-04 21:51:31 +0100

    chore: add environment variables for Google API key and backend URL in .env and .env.production files chore: add Firebase configuration files and hosting settings chore: remove frontend/.env and frontend/.gitignore files chore: add package.json file with dependencies and scripts chore: add favicon.ico and index.html files in public directory
    feat: add new files and components for places and public directories
    feat: add PlaceItem and PlaceList components for displaying places
    feat: add error handling and loading spinner to PlaceItem component
    feat: add confirmation modal for deleting a place in PlaceItem component
    feat: add conditional rendering for empty places list in PlaceList component
    fix: change ReactDOM.render to ReactDOM.createRoot in index.js
    style: add CSS files for PlaceItem and PlaceList components
    
    feat: add new components and styles for creating and updating places
    
    This commit adds the following new components and styles:
    - NewPlace.js: a component for creating a new place
    - UpdatePlace.js: a component for updating an existing place
    - UserPlaces.js: a component for displaying all places created by a user
    - PlaceForm.css: styles for the place creation and update forms
    - Button.js: a reusable button component with different styles
    - Button.css: styles for the button component
    - ImageUpload.css: styles for the image upload component
    
    The changes also include updates to the existing components to use the new components and styles. The changes allow users to create and update places, and view all places created by a user.
    
    feat: add new components for form input and image upload
    feat: add MainHeader and MainNavigation components for navigation
    fix: change MainNavigation component to use NavLink instead of Link
    style: add CSS for new components
    
    feat: add form validators and validation function
    
    Add form validators for required fields, file inputs, minimum and maximum length, minimum and maximum values, and email inputs. Also add a validation function that takes a value and an array of validators and returns a boolean indicating whether the value is valid according to the validators.
    
    feat: add new hooks for authentication, form handling, and http requests
    
    Add new hooks for authentication, form handling, and http requests to improve code reusability and simplify code structure. The `UseAuth` hook handles user authentication and token management, the `useForm` hook handles form input and validation, and the `useHttpClient` hook handles http requests and error handling. Also, add new CSS files for `UserItem`, `UsersList`, and `Auth` components to improve styling and layout.
    
    feat(Auth.js, Users.jsx): add user authentication and user list functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -6,14 +6,16 @@ import {
   Switch,
 } from "react-router-dom";
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/components/context/auth-context";
 import { UseAuth } from "./shared/hooks/auth";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+
+const Users = React.lazy(() => import("./user/pages/Users"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 const App = () => {
   const { token, login, logout, userId } = UseAuth();
@@ -66,7 +68,17 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
       </Router>
     </AuthContext.Provider>
   );
